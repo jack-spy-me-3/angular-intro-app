9ab3efd4d8f0797b994775ead441be13e854430c
Samantha Morco <samanthamorco@gmail.com>
2017-02-27 21:04:55 -0500

    complete todo app

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -4,15 +4,14 @@
 
   angular.module("app").controller("stuffCtrl", function($scope) {
     $scope.tasks = [
-                    {text: "Go on a diet", assignee: "James"},
-                    {text: "Bring the coffee", assignee: "Jonathan"},
-                    {text: "Help the students", assignee: "Andy"}
+                    {text: "Go on a diet", completed: false},
+                    {text: "Bring the coffee", completed: false},
+                    {text: "Help the students", completed: false}
                    ];
 
-    $scope.addTodo = function(todo, inputAssignee) {
+    $scope.addTodo = function(todo) {
       if (todo) {
-        $scope.tasks.push({text: todo,
-                      assignee: inputAssignee});
+        $scope.tasks.push({text: todo});
         $scope.newTodo = null;
       }
     };
@@ -21,8 +20,36 @@
       $scope.tasks.splice(index, 1);
     };
 
+    $scope.markComplete = function(task) {
+      task.completed = true;
+    };
+
+    $scope.counter = function() {
+      var count = 0;
+      for (var i = 0; i < $scope.tasks.length; i++) {
+        if (!$scope.tasks[i].completed) {
+          count++;
+        }
+      }
+      return count;
+    };
+
     $scope.isNotFood = function(task) {
       return task.indexOf("food") === -1;
+    };
+
+    $scope.removeCompletedTasks = function() {
+      var completedTasks = [];
+
+      for (var i = 0; i < $scope.tasks.length; i++) {
+        if ($scope.tasks[i].completed) {
+          completedTasks.push($scope.tasks[i]);
+        }
+      }
+
+      for (var j = 0; j < completedTasks.length; j++) {
+        $scope.tasks.splice($scope.tasks.indexOf(completedTasks[j]), 1);
+      }
     };
     window.$scope = $scope;
   });
